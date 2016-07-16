a5fcb2c43169687ba70655d69d17aee933011208
Colin Kiegel <kiegel@gmx.de>
2016-07-16 13:16:35 +0200

    REMOVED meetup-js for rsvp button due to bugs with chrome/safari -> https://github.com/meetup/api/issues/149

--- meetup.js
+++
@@ -1,3 +0,0 @@
-// http://www.meetup.com/de-DE/meetup_api/buttons/
-// include at bottom of page
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s); js.id=id;js.async=true;js.src="https://a248.e.akamai.net/secure.meetupstatic.com/s/script/2012676015776998360572/api/mu.btns.js?id=4c3q61r70aprf788m59plhg1cj";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","mu-bootjs");
